5a6b3443abea733a3e0244669cfed28f3a1c4cc5
John Doe <johndoe@example.com>
2023-06-03 08:57:03 +0530

    lolo

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className=" mx-auto h-screen m-auto">
       <h1 className="text-5xl font-semibold">Elangovan</h1>
+      <p>Background</p>
 
     </main>
   );
